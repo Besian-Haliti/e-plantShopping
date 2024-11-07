7282dcbc2dc9b35a6ba398de57eaf2997d4240fa
Besian-Haliti <besian.haliti@hotmail.com>
2024-11-06 20:21:36 -0500

    First Save

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,27 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+        const { name, image, cost } = action.payload;
+        const numericCost = parseFloat(cost.replace('$', ''));
 
-    
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+            existingItem.quantity++;
+        } else {
+            state.items.push({ name, image, cost: numericCost, quantity: 1 });
+        }
+    },
+
+    removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload.id);
+    },
+
+    updateQuantity: (state, action) => {
+        const { name, quantity } = action.payload;
+        const itemToUpdate = state.items.find(item => item.name === name);
+        if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+        }
     },
   },
 });

7b1d1da8f633818ca32d9ad77b0b097274b94049
Besian-Haliti <besian.haliti@hotmail.com>
2024-11-09 12:32:22 -0500

    final update

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -18,27 +18,29 @@ const CartItem = ({ onContinueShopping }) => {
     onContinueShopping(e);
   };
 
-
-
   const handleIncrement = (item) => {
     dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
     if (item.quantity > 1) {
-            dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
-        } else {
-            handleRemove();
-        }
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } 
+    else {
+        handleRemove(item);
+    }
   };
 
   const handleRemove = (item) => {
     dispatch(removeItem({ id: item.name }));
   };
 
-  // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
     return item.cost*item.quantity;
+  };
+
+  const handleCheckout = () => {
+    alert("The checkout feature is coming soon! Stay tuned for updates.");
   };
 
   return (
@@ -66,7 +68,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckout(e)}>Checkout</button>
       </div>
     </div>
   );
